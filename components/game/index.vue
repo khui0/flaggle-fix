17d9e944c4094f0605b634d02b10dde49629ca9c
Kenny <101839505+khui0@users.noreply.github.com>
2024-06-04 17:23:02 -0400

    feat: separate give up button from play again

--- components/game/index.vue
+++ components/game/index.vue
@@ -1,11 +1,21 @@
 <template>
-  <div
-    v-if="gameStore.selected"
-    class="game"
-  >
+  <div v-if="gameStore.selected" class="game">
+    <!-- Flag -->
     <div class="game__flag">
       <GameFlag :image="`images/flags/${gameStore.selected.flag}`" />
     </div>
+    <!-- Guess input -->
+    <div v-if="!gameOver || !displayAnswer" class="game__guess-input">
+      <GameGuessInput
+        :game-over="gameOver"
+        :game-mode="gameMode"
+        @answer="answer"
+      />
+    </div>
+    <div v-else class="game__answer">
+      <h3>Correct Answer: {{ gameStore.selected.name }}</h3>
+    </div>
+    <!-- Guess grid -->
     <div class="game__guesses">
       <GameGuessDisplay
         v-for="(result, x) in gameStore.results"
@@ -17,56 +27,38 @@
         :key="remaining"
       />
     </div>
-    <div class="game__restart">
-      <button
-        class="game__play-again"
-        @click="resetGame"
-      >
-        {{ gameOver ? 'Play Again' : 'I Give Up!' }}
-      </button>
+    <!-- Give up button -->
+    <div v-if="!displayAnswer && !gameOver" class="game__give-up-menu">
+      <button class="game__play-again" @click="giveUp">Give Up</button>
     </div>
-    <div
-      v-if="!gameOver || !displayAnswer"
-      class="game__guess-input"
-    >
-      <GameGuessInput
-        :game-over="gameOver"
-        :game-mode="gameMode"
-        @answer="answer"
-      />
-    </div>
-    <div
-      v-else
-      class="game__answer"
-    >
-      <h3>
-        Correct Answer: {{ gameStore.selected.name }}
-      </h3>
+    <!-- Play again button -->
+    <div v-if="gameOver">
+      <button class="game__play-again" @click="playAgain">Play Again</button>
     </div>
   </div>
 </template>
 
 <script>
-import Flagle from '@/utils/flagle';
+import Flagle from "@/utils/flagle";
 
 export default {
   props: {
     states: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
   data() {
     return {
       gameStore: gameStore(),
       gameOver: false,
-      displayAnswer: false
+      displayAnswer: false,
     };
   },
   computed: {
     gameMode() {
-      return this.states ? 'states' : 'countries';
-    }
+      return this.states ? "states" : "countries";
+    },
   },
   mounted() {
     // the user exited without starting a new game, so we start one
@@ -78,10 +70,10 @@ export default {
       }
     }
     this.startGame();
-    document.addEventListener('keydown', this.handleKeyDown);
+    document.addEventListener("keydown", this.handleKeyDown);
   },
   beforeUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
+    document.removeEventListener("keydown", this.handleKeyDown);
   },
   methods: {
     answer(guess) {
@@ -104,16 +96,8 @@ export default {
         this.gameStore.generateRandom(this.gameMode);
       }
     },
-    resetGame() {
-      if (!this.gameOver) {
-        // user is resetting before they finished, reset their streak
-        this.gameStore.statistics.winStreak = 0;
-        // record as a loss
-        this.gameStore.handleResult(false, this.gameStore.results.length);
-        // show the the correct answer
-        this.displayAnswer = true;
-        this.gameOver = true;
-      } else {
+    playAgain() {
+      if (this.gameOver) {
         // game has already been registerd as over, so we restart it
         this.gameOver = false;
         this.displayAnswer = false;
@@ -127,13 +111,32 @@ export default {
           input.focus();
         }
       }, 500);
-    }
-  }
+    },
+    giveUp() {
+      if (!this.gameOver) {
+        // user is resetting before they finished, reset their streak
+        this.gameStore.statistics.winStreak = 0;
+        // record as a loss
+        this.gameStore.handleResult(false, this.gameStore.results.length);
+        // show the the correct answer
+        this.displayAnswer = true;
+        this.gameOver = true;
+      }
+      // after the button click, focus the input, but give it 0.5s to render
+      setTimeout(() => {
+        const input = document.querySelector('input[type="text"]');
+        if (input) {
+          input.focus();
+        }
+      }, 500);
+    },
+  },
 };
 </script>
 
 <style lang="scss">
 .game {
+  position: relative;
   display: flex;
   flex-direction: column;
   gap: var(--base-small-gap);
@@ -161,6 +164,12 @@ export default {
         color: #fff;
       }
     }
+  }
+
+  &__give-up-menu {
+    position: absolute;
+    top: 0;
+    right: 0;
   }
 }
 </style>
