effda4ec6623cf31e4ae81c8abe7cc82d73a5229
Kenny <101839505+khui0@users.noreply.github.com>
2024-06-05 11:47:32 -0400

    fix: config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   ssr: false,
   app: {
-    baseURL: "/flagle/",
+    baseURL: "/flaggle-fix/",
     head: {
       titleTemplate: (titleChunk) => {
         return titleChunk ? `${titleChunk} | Flagle` : "Flagle";
